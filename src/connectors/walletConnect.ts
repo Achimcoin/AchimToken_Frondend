d4a877353f0d7e4136986938578d14ae0c9efb02
achimcoin <achimcoin.group@gmail.com>
2025-01-24 10:22:29 +0100

    React Swap

--- src/connectors/walletConnect.ts
+++ src/connectors/walletConnect.ts
@@ -1,8 +1,15 @@
 import { initializeConnector } from '@web3-react/core'
 import { Connector } from '@web3-react/types'
 import { WalletConnect } from '@web3-react/walletconnect'
+import { Buffer } from 'buffer'
+
 import { JSON_RPC_URL } from '../constants'
 import { toWeb3Connector } from './utils'
+
+// WalletConnect relies on Buffer, so it must be polyfilled.
+if (!('Buffer' in window)) {
+  window.Buffer = Buffer
+}
 
 export function isWalletConnect(connector: Connector) {
   return connector instanceof WalletConnect
